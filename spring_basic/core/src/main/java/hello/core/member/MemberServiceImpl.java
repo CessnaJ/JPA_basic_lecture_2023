81a574695683421497ee6e44fdf6e85500c3779c
CessnaJ <eon8748@gmail.com>
2023-04-24 18:02:41 +0900

    temp

--- spring_basic/core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ spring_basic/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,8 +1,16 @@
 package hello.core.member;
 
+import hello.core.discount.DiscountPolicy;
+
 public class MemberServiceImpl implements MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    public MemberServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public void join(Member member) {
